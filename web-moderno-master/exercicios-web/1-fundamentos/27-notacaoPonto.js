1c4713199c5b735e2783509bbaf0df8f472ead56
sandrosa1 <sandro.sa@fatec.sp.gov.br>
2021-07-20 20:56:28 -0300

    destructuring

--- web-moderno-master/exercicios-web/1-fundamentos/27-notacaoPonto.js
+++ web-moderno-master/exercicios-web/1-fundamentos/27-notacaoPonto.js
@@ -2,8 +2,9 @@ console.log(Math.ceil(6.1))
 
 const obj1 = {}
 obj1.nome = 'Bola'
-// obj1['nome'] = 'Bola2'
+// obj1['nome'] = 'Bola2' //Forma alternativa
 console.log(obj1.nome)
+
 
 function Obj(nome) {
     this.nome = nome
